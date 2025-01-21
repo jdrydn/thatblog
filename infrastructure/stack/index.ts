b66ff8ed499466f285929f696322c1573768c055
jdrydn <james@jdrydn.com>
2025-01-21 22:06:00 +0000

    fix: Added deploy script & deploy command

--- infrastructure/stack/index.ts
+++ infrastructure/stack/index.ts
@@ -1,22 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 
 export default function createStack(app: cdk.App, stackName: string) {
-  const stack = new cdk.Stack(app, 'Stack', {
+  const stack = new cdk.Stack(app, 'Thatblog', {
     stackName,
   });
 
+  const suffix = cdk.Fn.select(4, cdk.Fn.split('-', cdk.Fn.select(2, cdk.Fn.split('/', cdk.Fn.ref('AWS::StackId')))));
+
   const bucket = new cdk.aws_s3.Bucket(stack, 'Files', {
+    bucketName: cdk.Fn.join('-', [stackName, suffix]),
     encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
     publicReadAccess: false, // Optional: Only if you want the assets to be publicly accessible
     removalPolicy: cdk.RemovalPolicy.DESTROY, // Optional: Cleanup bucket on stack deletion
-    autoDeleteObjects: true, // Optional: Delete objects when stack is deleted
-  });
-
-  // Deploy static assets to the bucket
-  new cdk.aws_s3_deployment.BucketDeployment(stack, 'DeployStaticAssets', {
-    sources: [cdk.aws_s3_deployment.Source.asset('./dist')],
-    destinationBucket: bucket,
-    destinationKeyPrefix: 'frontend', // Deploy to the `/frontend` folder in the bucket
   });
 
   // Output the bucket name
---
+++ scripts/deploy.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+DIR="$( cd -- "$(dirname "$0")/../" >/dev/null 2>&1 ; pwd -P )"
+
+ARG_STACKNAME=""
+ARG_DESTROY=false
+ARG_RENEW=false
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    "--stack-name") ARG_STACKNAME="$2"; shift ;;
+    "--aws-profile") export AWS_PROFILE="$2"; shift ;;
+    "--aws-region") export AWS_REGION="$2"; shift ;;
+    "--destroy") ARG_DESTROY=true ;;
+    "--renew") ARG_DESTROY=true; ARG_RENEW=true ;;
+  esac
+  shift
+done
+
+throw_err() {
+  if [ "$1" -ne "0" ]; then
+    echo "[ERROR] $2" 1>&2
+    exit "$1"
+  fi
+}
+
+echo $DIR
+
+aws --version >/dev/null
+throw_err "$?" "Missing CLI: aws"
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+throw_err "$?" "Failed to authenticate with AWS"
+
+BUCKET_QUERY="Stacks[?StackName=='$ARG_STACKNAME'][].Outputs[?OutputKey=='BucketName'].OutputValue"
+
+if [ "$ARG_TF_DESTROY" = "true" ]; then
+  BUCKET_NAME=$(aws cloudformation describe-stacks --query "$BUCKET_QUERY" --output text)
+  echo "[TASK] Clearing bucket: $BUCKET_NAME"
+  aws s3 rm "s3://$BUCKET_NAME" --recursive
+  throw_err "$?" "Failed to empty S3 bucket"
+
+  cd $DIR/infrastructure
+  npx cdk destroy -c stackName="$ARG_STACKNAME"
+  throw_err "$?" "Failed: cdk destroy"
+
+  # If we destroyed the stack, exit early
+  if [ "$ARG_RENEW" = "false" ]; then
+    exit 0
+  fi
+
+  printf "\n\n"
+fi
+
+cd $DIR/infrastructure
+npx cdk deploy -c stackName="$ARG_STACKNAME"
+throw_err "$?" "Failed: cdk deploy"
+
+cd $DIR
