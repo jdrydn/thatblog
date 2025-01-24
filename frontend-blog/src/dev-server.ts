28d5b18a03336ddc8cc2b1a63cde1a8246b47d08
jdrydn <james@jdrydn.com>
2025-01-24 21:21:14 +0000

    refactor: Deploying the function to AWS

--- frontend-blog/src/dev-server.ts
+++ frontend-blog/src/dev-server.ts
@@ -1,4 +1,4 @@
-import { app } from '../src/app';
+import { app } from './app';
 
 app.listen(3000, 'localhost', () => {
   console.log('[thatblog][frontend-blog] Listening on http://localhost:3000');
--- infrastructure/stack/index.ts
+++ infrastructure/stack/index.ts
@@ -1,8 +1,16 @@
+import path from 'path';
 import * as cdk from 'aws-cdk-lib';
 
 export default function createStack(app: cdk.App, stackName: string) {
   const stack = new cdk.Stack(app, 'Thatblog', {
     stackName,
+  });
+
+  const siteLambda = new cdk.aws_lambda.Function(stack, 'SiteLambda', {
+    functionName: `${stackName}-site`,
+    runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+    handler: 'app-min.handler',
+    code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, '../../frontend-blog/dist')),
   });
 
   const suffix = cdk.Fn.select(4, cdk.Fn.split('-', cdk.Fn.select(2, cdk.Fn.split('/', cdk.Fn.ref('AWS::StackId')))));
@@ -15,9 +23,13 @@ export default function createStack(app: cdk.App, stackName: string) {
   });
 
   // Output the bucket name
+  new cdk.CfnOutput(stack, 'SiteLambdaFunction', {
+    description: 'Name of the Site Lambda function',
+    value: siteLambda.functionName,
+  });
   new cdk.CfnOutput(stack, 'BucketName', {
-    value: bucket.bucketName,
     description: 'Name of the S3 bucket',
+    value: bucket.bucketName,
   });
 
   return stack.stackName;
