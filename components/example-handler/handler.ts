51b52d70c267080f9cb1a44975129ebdf84e7e56
jdrydn <james@jdrydn.com>
2026-04-15 22:31:30 +0100

    fix: Serverless deployment

--- components/example-handler/handler.ts
+++ components/example-handler/handler.ts
@@ -1,13 +1,7 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-
-const {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} = require("@aws-sdk/lib-dynamodb");
-
-const express = require("express");
-const serverless = require("serverless-http");
+import express, { Request, Response } from 'express';
+import serverless from 'serverless-http';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, GetCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 
 const app = express();
 
@@ -17,7 +11,7 @@ const docClient = DynamoDBDocumentClient.from(client);
 
 app.use(express.json());
 
-app.get("/users/:userId", async (req, res) => {
+app.get('/users/:userId', async (req: Request, res: Response) => {
   const params = {
     TableName: USERS_TABLE,
     Key: {
@@ -32,21 +26,19 @@ app.get("/users/:userId", async (req, res) => {
       const { userId, name } = Item;
       res.json({ userId, name });
     } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
+      res.status(404).json({ error: 'Could not find user with provided "userId"' });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Could not retrieve user" });
+    res.status(500).json({ error: 'Could not retrieve user' });
   }
 });
 
-app.post("/users", async (req, res) => {
+app.post('/users', async (req: Request, res: Response) => {
   const { userId, name } = req.body;
-  if (typeof userId !== "string") {
+  if (typeof userId !== 'string') {
     res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== "string") {
+  } else if (typeof name !== 'string') {
     res.status(400).json({ error: '"name" must be a string' });
   }
 
@@ -61,14 +53,14 @@ app.post("/users", async (req, res) => {
     res.json({ userId, name });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Could not create user" });
+    res.status(500).json({ error: 'Could not create user' });
   }
 });
 
-app.use((req, res, next) => {
+app.use((_req: Request, res: Response) => {
   return res.status(404).json({
-    error: "Not Found",
+    error: 'Not Found',
   });
 });
 
-exports.handler = serverless(app);
+export const handler = serverless(app);
