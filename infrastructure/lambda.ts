0b32b8e2c32f030a9da49c965e975f5f35e5aa50
jdrydn <james@jdrydn.com>
2026-04-19 00:12:45 +0100

    refactor: Improving handler

--- infrastructure/lambda.ts
+++ infrastructure/lambda.ts
@@ -1,19 +1,20 @@
-import middy from '@middy/core';
-import httpRouterHandler from '@middy/http-router';
-import httpHeaderNormalizer from '@middy/http-header-normalizer';
+import type { APIGatewayProxyEventV2, Context } from 'aws-lambda';
 
-import { routes as backendApiRoutes } from '@thatblog/backend-api/handler';
-import { routes as frontendAppRoutes } from '@thatblog/frontend-app/handler';
-import { withPrefix } from '@thatblog/middy-helpers/routes';
-import type { RequestEvent, ResponseResult } from '@thatblog/middy-helpers/types';
+import { handler as backendApiHandler } from '@thatblog/backend-api/handler';
+import { handler as frontendAppHandler } from '@thatblog/frontend-app/handler';
 
-export const handler = middy<RequestEvent, ResponseResult>()
-  .use(httpHeaderNormalizer())
-  .handler(
-    httpRouterHandler([
-      // API routes prefixed accordingly
-      ...withPrefix('/api', backendApiRoutes),
-      // Regular frontend app routes
-      ...frontendAppRoutes,
-    ]),
-  );
+/**
+ * Handler for incoming HTTP requests
+ */
+export function http(event: APIGatewayProxyEventV2, context: Context) {
+  if (event.rawPath.startsWith('/api')) {
+    return backendApiHandler(event, context);
+  } else {
+    return frontendAppHandler(event, context);
+  }
+}
+
+/**
+ * Handler for incoming Eventbridge events
+ */
+// export function worker()
